f0ccddaaf1812955a61ce6b024f2010e81db097b
danielnyvoll <danielnyvoll@hotmail.com>
2022-02-01 19:37:56 +0100

    Added all files

---
+++ src/FPLprices.svelte
@@ -0,0 +1,12 @@
+<script>
+	let person;
+	
+	const hentPerson = async () => {
+		const data = await fetch('https://fantasy.premierleague.com/api/bootstrap-static/')
+		const json = await data.json()
+		person = json
+	}
+	console.log(hentPerson());
+
+</script>
+
